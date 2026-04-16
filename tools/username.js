fb2ef64933dcf1ddd221dfb633c5929510659db3
avonci <janvoncina05@gmail.com>
2026-04-16 11:49:27 +0200

    Update username.js

--- tools/username.js
+++ tools/username.js
@@ -1,10 +1,14 @@
 function generate() {
     const adjectives = [
-        "cool", "fast", "dark", "silent", "crazy", "smart", "wild", "jewish", "black", "retardet", "white", "the", "stupid", "zesty", "stinky", "smelly", "small", "big", "useless"
+        "cool", "fast", "dark", "silent", "crazy", "smart", "wild", "jewish", "black", "retardet", "white", "the", "stupid", "zesty", "stinky", "smelly", "small", "big", "useless", 
+        "sexy", "horny", "autistic", "beautiful", "ugly", "long", "short", "twisted", "curved", "straight", "rich", "old", "young", "fat", "fugly", "drunk", "sneaky", "spicy", "salty", 
+        "massive", "sticky", "slimy", "dirty", "hairy", "bouncy", "radioactive", "broken", "invisible", "fluffy", "tiny", "dusty", "rusty"
     ];
 
     const nouns = [
-        "tiger", "ghost", "ninja", "lion", "dragon", "wolf", "shadow", "nigger", "fucker", "assassin", "nigga", "retard", "jew", "fuck", "cunt", "digga", "nazi", "goat", "G.O.A.T", "chonker", "chungus", "dick", "balls", "faggot", "gayboy", "ladyboy", "šilček"
+        "tiger", "ghost", "ninja", "lion", "dragon", "wolf", "shadow", "nigger", "fucker", "assassin", "nigga", "retard", "jew", "fuck", "cunt", "digga", "nazi", "goat", "G.O.A.T", 
+        "chonker", "chungus", "dick", "balls", "faggot", "gayboy", "ladyboy", "šilček", "titties", "asscrack", "dildo", "motherfucker", "loverboy", "romantic", "soldier", "newborn", 
+        "fatass", "bitch", "addict", "alcoholic", "monkey", "donkey", "goblin", "wallbreaker", "muslim", "terrorist", "sausage", "boner", "microwawe", "officer"
     ];
 
     const useNumbers = document.getElementById("numbers").checked;
