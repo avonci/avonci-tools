a4a21e0429f5619a94fc529ed614ffc928320f78
avonci <janvoncina05@gmail.com>
2026-04-20 20:27:08 +0200

    Update username.js

--- tools/username.js
+++ tools/username.js
@@ -3,26 +3,41 @@ function generate() {
     const adjStandard = [
         "cool", "fast", "dark", "silent", "crazy", "smart", "wild", "the", "small", "big", 
         "beautiful", "ugly", "long", "short", "rich", "old", "young", "sneaky", "spicy", 
-        "massive", "bouncy", "radioactive", "broken", "invisible", "fluffy", "tiny", "rusty"
+        "massive", "bouncy", "radioactive", "broken", "invisible", "fluffy", "tiny", "rusty",
+        "cheesy", "chaotic", "feral", "cursed", "blessed", "greasy", "wobbly", "crusty",
+        "derpy", "glitchy", "toasted", "burnt", "sleepy", "grumpy", "salty", "austrian",
+        "unhinged", "shady", "suspicious", "sticky", "slimy", "dusty", 
+        "electric", "cosmic", "haunted", "frozen", "ferocious", "ancient",
+        "overcaffeinated", "underpaid", "confused", "unstable", "goofy",
+        "funky", "sassy", "messy", "noisy", "hyper", "cranky", "laggy",
+        "pixelated", "mutated", "toxic", "gremlin", "doomed", "caffeinated"
     ];
 
     const nounStandard = [
         "tiger", "ghost", "ninja", "lion", "dragon", "wolf", "shadow", "assassin", "goat", 
         "G.O.A.T", "chonker", "chungus", "romantic", "soldier", "monkey", "donkey", "goblin", 
-        "wallbreaker", "sausage", "microwave", "officer"
+        "wallbreaker", "sausage", "microwave", "officer", "ball", "potato", "pickle", "waffle", 
+        "toaster", "donut", "burrito", "raccoon", "pigeon", "hamster", "penguin", "frog", "lizard",
+        "dumpster", "keyboard", "cactus", "blob", "meme", "disaster", "gremlin", "dumpster",
+        "banana", "noodle", "sandwich", "croissant", "cheesecake",
+        "wizard", "pirate", "zombie", "skeleton", "robot", "alien",
+        "bug", "glitch", "pixel", "virus", "void", "meteor",
+        "gargoyle", "troll", "rat", "goblinking", "meatball", "shrimp", "painter"
     ];
 
     // EXPLICIT WORDS
     const adjExplicit = [
         "jewish", "black", "retardet", "white", "stupid", "zesty", "stinky", "smelly", "useless", 
-        "sexy", "horny", "autistic", "fat", "fugly", "drunk", "salty", "sticky", "slimy", "dirty", "hairy"
+        "sexy", "horny", "autistic", "fat", "fugly", "drunk", "salty", "sticky", "slimy", "dirty", 
+        "hairy", "smelly", "brown", "indian", 
     ];
 
     const nounExplicit = [
         "nigger", "fucker", "nigga", "retard", "jew", "fuck", "cunt", "digga", "nazi", "dick", 
         "balls", "faggot", "gayboy", "ladyboy", "šilček", "titties", "asscrack", "dildo", 
-        "motherfucker", "loverboy", "fatass", "bitch", "addict", "alcoholic", 
-        "muslim", "terrorist", "boner"
+        "motherfucker", "loverboy", "fatass", "bitch", "addict", "alcoholic", "gangster",
+        "muslim", "terrorist", "boner", "taliban", "tits", "penis", "buttplug", "balls",
+        "rapist"
     ];
 
     // GET OPTIONS
